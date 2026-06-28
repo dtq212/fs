4545e3f0de216b9900d8328310332131127e1d53
Thọ Phạm <dtq21295@gmail.com>
2026-06-28 11:05:06 +0700

    Phien ban moi

--- loop.py
+++ loop.py
@@ -76,34 +76,6 @@ class LoopLamMoiTrangThaiTacTu:
         self.tactu.action_lammoitrangthaitactu()
         self.tactu.action_kiemtraxulyloitudongtimduong()
 
-        thoidiem_hientai = time.time()
-        toado_hientai = self.moitruong.get_toado()
-        idtrangthai_hientai = self.moitruong.get_idtrangthainhanvat()
-
-        if self.moitruong.get_tennhanvat() == "OsamaDaEĐen" and toado_hientai != (-1, -1) and idtrangthai_hientai == IDTRANGTHAINHANVAT_DICHUYEN:
-            is_bidongbang = self.moitruong.get_is_bidongbang()
-            tocdo_game = self.moitruong.get_tocdodichuyen()
-
-            if self.thongtindichuyen_banthan:
-                thoidiem_cu, toado_cu = self.thongtindichuyen_banthan
-                delta_t = thoidiem_hientai - thoidiem_cu
-
-                if delta_t > 0.1:
-                    khoangcach_thucte = math.dist(toado_hientai, toado_cu)
-                    tocdo_thucte = khoangcach_thucte / delta_t
-
-                    print(f"--- THÔNG SỐ ĐO ĐẠC BẢN THÂN ---")
-                    print(f" Trạng thái đóng băng: {is_bidongbang}")
-                    print(f" Tốc độ hiển thị trong game: {tocdo_game}")
-                    print(f" Quãng đường chạy được: {khoangcach_thucte:.2f} pixel (trong {delta_t:.2f}s)")
-                    print(f" Vận tốc thực tế (pixel/giây): {tocdo_thucte:.2f} px/s")
-
-                    self.thongtindichuyen_banthan = (thoidiem_hientai, toado_hientai)
-            else:
-                self.thongtindichuyen_banthan = (thoidiem_hientai, toado_hientai)
-        else:
-            self.thongtindichuyen_banthan = None
-
 class LoopPhu:
     def __init__(self, moitruong: MoiTruong, tactu: TacTu, stop: threading.Event):
         self.moitruong = moitruong
